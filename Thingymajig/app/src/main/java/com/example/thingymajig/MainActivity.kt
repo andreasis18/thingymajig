5cc70f6d12f8e325698a15a6ee1236806f1b20bd
Nadares <andreasivan181@gmail.com>
2019-12-12 20:02:59 +0700

    Up gradle

--- Thingymajig/app/src/main/java/com/example/thingymajig/MainActivity.kt
+++ Thingymajig/app/src/main/java/com/example/thingymajig/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.thingymajig
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TabLayout
+import android.util.Log
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +24,48 @@ class MainActivity : AppCompatActivity() {
         scrollView.isFillViewport = true
 
         var q = Volley.newRequestQueue(this)
-        val url = "http://penir.jitusolution.com"
-        val stringRequest = StringRequest(Request.Method.GET, url, Response.Listener<String>{}, Response.ErrorListener {})
+        val url = Generator.url
+
+        val stringRequest = object: StringRequest(Request.Method.POST, url, Response.Listener<String>{
+                response-> try{
+            Log.e("resp", response)
+            val jsonObject = JSONObject(response)
+            val jsonArray: JSONArray = jsonObject.getJSONArray("projects")
+            for(i in 0 until jsonArray.length()){
+                val jsonProject = jsonArray.getJSONObject(i)
+                projects.add(Project(jsonProject.getInt("id"), jsonProject.getString("project_name"),
+                    jsonProject.getString("leader"), jsonProject.getString("project_status")))
+
+                if(Global.loggedUser == jsonProject.getString("leader")){
+                    projectsLead.add(Project(jsonProject.getInt("id"), jsonProject.getString("project_name"),
+                        jsonProject.getString("leader"), jsonProject.getString("project_status")))
+                }
+                else{
+                    projectsParticipating.add(Project(jsonProject.getInt("id"), jsonProject.getString("project_name"),
+                        jsonProject.getString("leader"), jsonProject.getString("project_status")))
+                }
+            }
+
+        } catch (e: JSONException){
+            Log.e("error",e.message)
+        }
+        }, Response.ErrorListener{
+                response-> Toast.makeText(this, response.toString(), Toast.LENGTH_SHORT).show()
+        })
+        {
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params.put("username", Generator.loggedUser)
+                return params
+            }
+        }
+        q.add(stringRequest)
+
+        val fragmentAdapter = FragmentAdapter(supportFragmentManager)
+        fragmentAdapter.addFragment(FragFavRecipe())
+        fragmentAdapter.addFragment(FragListRecipe())
+        fragmentAdapter.addFragment(FragAddRecipe())
+        viewPager.adapter = fragmentAdapter
 
         tabs.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
             override fun onTabReselected(tab: TabLayout.Tab?) {
